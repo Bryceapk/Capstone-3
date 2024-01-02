987acdc023eb8e9cfddb203b566c8dea0d0b4239
Bryceapk <Bryceapk1@gmail.com>
2024-01-02 10:43:10 -0600

    Commit

---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const loginForm = document.querySelector("#login");
+
+loginForm.onsubmit = function (event) {
+
+    event.preventDefault();
+
+    // We can use loginForm.username (for example) to access
+    // the input element in the form which has the ID of "username".
+    const loginData = {
+        username: loginForm.username.value,
+        password: loginForm.password.value,
+    }
+
+    // Disables the button after the form has been submitted already:
+    loginForm.loginButton.disabled = true;
+
+    // Time to actually process the login using the function from auth.js!
+    login(loginData);
+};
